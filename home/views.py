cfea0db82062668889475e7aad6bceea2c05726d
zohaib <89574767+zohaib000@users.noreply.github.com>
2021-09-08 01:02:37 -0700

    Update views.py

--- home/views.py
+++ home/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from home.models import img
 # Create your views here.
 def image(request):
-    n=range(1,101)
+    n=range(1,5)
     return render(request,"home/search.html",{"n":n})
