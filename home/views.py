4317da53c9e2f0d25db59f44ba4f43878e3449b7
zohaib <89574767+zohaib000@users.noreply.github.com>
2021-09-08 19:56:09 -0700

    Update views.py

--- home/views.py
+++ home/views.py
@@ -3,4 +3,8 @@ from home.models import img
 # Create your views here.
 def image(request):
     n=range(0,91)
-    return render(request,"home/search.html",{"n":n})
+    return render(request,"home/search.html")
+
+def searching(request):
+    n=range(0,91)
+    return render(request,"home/searching.html",{"n":n})
