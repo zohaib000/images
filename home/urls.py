df1e1f925f88720f0aa7de0611a75f623a517e6f
zohaib <89574767+zohaib000@users.noreply.github.com>
2021-09-09 20:41:30 -0700

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,15 @@ from django.urls import path,include
 from home import views
 
 urlpatterns = [
-    path('', views.image,name="image"),
-    path("searching",views.searching,name="searching"),
-    path("joker",views.joker,name="joker")
+     path('', views.image,name="image"),
+     path("searching",views.searching,name="searching"),
+     path("joker",views.joker,name="joker"),
+     path("pig",views.pig,name="pig"),
+     path("birds",views.birds,name="birds"),
+     path("lion",views.lion,name="lion"),
+     path("tiger",views.tiger,name="tiger"),
+     path("fifa",views.fifa,name="fifa"),
+     path("poetry",views.poetry,name="poetry"),
+     path("animals",views.animals,name="animals"),
+     path("parrot",views.parrot,name="parrot"),
 ]
